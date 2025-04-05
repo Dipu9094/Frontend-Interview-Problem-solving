ceca7b9d4eb1b2df2209b3bf09dd3c3ca7431fc3
Dipu9094 <dipusultan090@gmail.com>
2025-04-05 23:04:30 +0200

    HashMapObjSet

---
+++ HashMapObjSet.js
@@ -0,0 +1,15 @@
+function firstUniqueChar(str) {
+    let map = {};
+
+    for (let char of str) {
+        map[char] = (map[char] || 0) + 1;
+    }
+
+    for (let char of str) {
+        if (map[char] === 1) return char;
+    }
+
+    return null;
+}
+
+console.log(firstUniqueChar("aabccdeff")); // "b"
