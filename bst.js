5a2dbd50bc8ad6d90246728ea45f31e9800ee63d
Dipu9094 <dipusultan090@gmail.com>
2025-04-10 23:36:30 +0200

    bst

---
+++ bst.js
@@ -0,0 +1,47 @@
+class Node {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }
+  
+  class BinarySearchTree {
+    constructor() {
+      this.root = null;
+    }
+  
+    insert(value) {
+      const newNode = new Node(value);
+      if (!this.root) {
+        this.root = newNode;
+        return;
+      }
+  
+      let current = this.root;
+      while (true) {
+        if (value < current.value) {
+          if (!current.left) {
+            current.left = newNode;
+            return;
+          }
+          current = current.left;
+        } else {
+          if (!current.right) {
+            current.right = newNode;
+            return;
+          }
+          current = current.right;
+        }
+      }
+    }
+  }
+  
+  // Usage
+  const bst = new BinarySearchTree();
+  bst.insert(10);
+  bst.insert(5);
+  bst.insert(15);
+  
+  console.log(bst);
+  
