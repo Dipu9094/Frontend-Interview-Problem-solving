23bc5f0696281233fd117071994d1bbef1f15c92
Dipu9094 <dipusultan090@gmail.com>
2025-04-18 17:23:38 +0200

    Maximum Subarray (Kadane's Algorithm)

---
+++ KadanesAlgo.js
@@ -0,0 +1,14 @@
+function maxSubArray(nums) {
+    let max = nums[0];
+    let current = nums[0];
+  
+    for (let i = 1; i < nums.length; i++) {
+      current = Math.max(nums[i], current + nums[i]);
+      max = Math.max(max, current);
+    }
+  
+    return max;
+  }
+  
+  console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])); // 6
+  
