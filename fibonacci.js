8908d38c0a0cf0b9f09adbd6ae711752fb753b71
Dipu9094 <dipusultan090@gmail.com>
2025-04-20 23:48:31 +0200

    fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,9 @@
+function fib(n, memo = {}) {
+    if (n <= 1) return n;
+    if (memo[n]) return memo[n];
+  
+    return memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  }
+  
+  console.log(fib(10)); // 55
+  
